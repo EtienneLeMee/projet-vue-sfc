47f6ad9010fdf2b541cd6d6d9e8f76bebbb084cf
pageAccueil <yohaneendoke4@gmail.com>
2023-01-05 15:12:28 +0100

    rien cassé pour l'instant

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SpellList spellmsg
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import SpellList from "@/components/SpellList";
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    HelloWorld,
+    SpellList,
   }
 }
 </script>
